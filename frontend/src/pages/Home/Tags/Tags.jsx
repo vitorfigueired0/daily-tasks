409315283e4227242cd7885693385ec2d318d88d
Vitor Figueiredo <vittor.figueiredo17@gmail.com>
2024-11-02 17:02:08 -0300

    [FE] Tag page layout

--- frontend/src/pages/Home/Tags/Tags.jsx
+++ frontend/src/pages/Home/Tags/Tags.jsx
@@ -3,6 +3,10 @@ import "./Tags.css";
 import { useCallback, useState } from "react";
 import PropTypes from "prop-types";
 import { MdOutlineDeleteOutline } from "react-icons/md";
+import Table from "../../../components/Table/Table"
+import Button from "../../../components/Button/Button"
+import InputText from "../../../components/InputText/InputText";
+
 export default function Tags({ tags, setTags }) {
   const [newTag, setNewTag] = useState("");
 
@@ -34,58 +38,24 @@ export default function Tags({ tags, setTags }) {
     setNewTag(event.target.value);
   };
 
-  const { headers, rows } = tags;
-
   return (
     <div className="tags-wrapper">
       <h1>Tags</h1>
       <div className="form-wrapper">
         <form onSubmit={(event) => handleTagSubmit(event)}>
-          <input
-            type="text"
-            placeholder="Enter tag name"
-            required
+          <InputText
+            placeholder={"Insert tag name"}
+            required={true}
             onChange={handleChange}
           />
-          <button type="submit">
-            <FaPlus /> Add Tag
-          </button>
+          <Button typeSubmit={true}>
+            <FaPlus />
+            Add Task
+          </Button>
         </form>
       </div>
-      <table>
-      <thead>
-        <tr>
-          {headers.map((header, index) => (
-            <th key={index}>
-              {header.label}
-            </th>
-          ))}
-          {rows.length !== 0 && <th ></th>}
-        </tr>
-      </thead>
-      <tbody>
-        {rows.length === 0 ? (
-          <tr>
-            <td colSpan={headers.length}></td>
-          </tr>
-        ) : (
-          rows.map((row) => (
-            <tr key={row.id}>
-              {headers.map((header, colIndex) => (
-                <td key={colIndex} style={{border: "1px solid black"}}>
-                  {row[header.column]}
-                </td>
-              ))}
-              <td style={{width: "1rem", border: "1px solid black"}}>
-                <button onClick={() => handleDeleteRow(row.id)}>
-                  <MdOutlineDeleteOutline />
-                </button>
-              </td>
-            </tr>
-          ))
-        )}
-      </tbody>
-    </table>
+
+      <Table data={tags} handleDeleteRow={handleDeleteRow}/>
     </div>
   );
 }
