de772d8d352e495740121cf3434690a8ed015bbb
Vitor Figueiredo <vittor.figueiredo17@gmail.com>
2024-11-05 14:29:48 -0300

    [FE] Creating tasks with tags

--- frontend/src/pages/Home/Board/Board.jsx
+++ frontend/src/pages/Home/Board/Board.jsx
@@ -8,15 +8,44 @@ import SingleSelector from "../../../components/OptionSelect/SingleSelector";
 import { KanbanBoard } from "../../../components/Kanban/KanbanBoard/KanbanBoard";
 import PropTypes from "prop-types";
 import { api } from "../../../services/api";
+import Select from 'react-select'
 
 export default function Board({ tasks, setTasks, status, tags }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [tagOptions, setTagOptions] = useState([])
+
+  useEffect(() => {
+    const mappedTags = tags.map((tag) => { return { value: tag.id, label: tag.name } })
+    setTagOptions(mappedTags)
+  }, [tags]);
+
+  const selectorStyles = {
+    container: (provided) => ({
+      ...provided,
+      height: '40px'
+      
+    }),
+    control: (provided) => ({
+     ...provided,
+      background: 'rgba(238, 242, 245, 0.50)',
+      padding: '5px 3px',
+      border: 'none',
+      borderRadius: '.5rem',
+      outline: '1px solid #E9EBEF',
+    }),
+    menu: (provided) => ({
+      ...provided,
+      background: 'white',
+    }),
+};
+
   const clearNewTask = {
     title: "",
     description: "",
     status: "",
     tags: []
   };
+
   const [newTask, setNewTask] = useState(clearNewTask);
 
   const handleCloseModal = () => {
@@ -43,11 +72,15 @@ export default function Board({ tasks, setTasks, status, tags }) {
   );
 
   const handleSubmit = async () => {
+    const mappedTags = newTask.tags.map((tag) => {
+      return { tagId: tag.value }
+    })
+    
     const data = {
       title: newTask.title,
       description: newTask.description,
       status: newTask.status,
-      tags: newTask.tags
+      tags: mappedTags
     }
 
     try {
@@ -76,7 +109,7 @@ export default function Board({ tasks, setTasks, status, tags }) {
         Add Task
       </Button>
 
-      <KanbanBoard tasks={tasks} setTasks={setTasks}/>
+      <KanbanBoard tasks={tasks} setTasks={setTasks} />
       <Modal
         isCreate={true}
         isOpen={isModalOpen}
@@ -93,6 +126,7 @@ export default function Board({ tasks, setTasks, status, tags }) {
             setNewTask((prev) => ({ ...prev, title: e.target.value }))
           }
         />
+        
         <InputText
           required={true}
           label="Description"
@@ -102,16 +136,27 @@ export default function Board({ tasks, setTasks, status, tags }) {
             setNewTask((prev) => ({ ...prev, description: e.target.value }))
           }
         />
-        <SingleSelector
-          label="Status"
-          required={true}
-          placeholder={"Select status"}
+        
+        <label>Status</label>
+        <Select
+          styles={selectorStyles}
           options={status}
-          onChange={(e) =>
-            setNewTask((prev) => ({ ...prev, status: e.target.value }))
+          defaultValue={status[0]}
+          onChange={(e) => 
+            setNewTask((prev) => ({ ...prev, status: e.value}))
           }
         />
-      
+        
+        <label>Tags</label>
+        <Select
+          styles={selectorStyles}
+          options={tagOptions}
+          isMulti
+          onChange={(values) => 
+            setNewTask((prev) => ({ ...prev, tags: values}))
+          }
+        />
+
       </Modal>
     </div>
   );
