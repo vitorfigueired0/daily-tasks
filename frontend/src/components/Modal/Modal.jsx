ade8ee0c55649753b2410c414d936daa7574fbff
Vitor Figueiredo <vittor.figueiredo17@gmail.com>
2024-11-05 16:54:03 -0300

    [FE] Task status update fix

--- frontend/src/components/Modal/Modal.jsx
+++ frontend/src/components/Modal/Modal.jsx
@@ -4,39 +4,63 @@ import { MdClose } from "react-icons/md";
 import Button from "../Button/Button";
 import { FaPlus } from "react-icons/fa";
 import { FiEdit2 } from "react-icons/fi";
-import SingleSelector from "../OptionSelect/SingleSelector";
+import Select from 'react-select'
 import { api } from "../../services/api"
+import { TagBadge } from "../TagBadge/TagBadge";
+import { useEffect, useState } from "react";
 
 export default function Modal({ isCreate, data, isOpen, onClose, children, title, handleSubmit, setTasks }) {
-  
+  const [statusLabel, setStatusLabel] = useState({ value: 'a', label: 'c'})
+
   if (!isOpen) {
     return null;
   }
 
-  const statusOptions = [
-    { id: 1, value: "pending", label: "To do" }, 
-    { id: 2, value: "inProgress", label: "In Progress"},
-    { id: 3, value: "completed", label: "Done" }
-  ];
+  const modalSelectorStyles = {
+    container: (provided) => ({
+      ...provided,
+      height: '40px'
 
+    }),
+    control: (provided) => ({
+      ...provided,
+      background: 'rgba(238, 242, 245, 0.50)',
+      padding: '5px 3px',
+      border: 'none',
+      borderRadius: '.5rem',
+      outline: '1px solid #E9EBEF',
+    }),
+    menu: (provided) => ({
+      ...provided,
+      background: 'white',
+    }),
+  };
+
+  const status = [
+    ['pending', { value: "pending", label: "To do" }], 
+    ['inProgress', { value: "inProgress", label: "In Progress" }], 
+    ['completed', { value: "completed", label: "Done" }]
+  ]
+
+  const statusOptions = new Map(status)
   const onSubmit = async (event) => {
     event.preventDefault();
     const success = await handleSubmit();
 
-    if(success) {
+    if (success) {
       onClose();
     }
   };
 
   const updateTaskStatus = async (id, status) => {
-    const data = { status }
+    const data = { status: status.value }
     try {
       await api.patch(`/tasks/${id}`, data, {
         headers: {
           Authorization: localStorage.getItem('authToken')
         }
       });
-      
+
       const response = await api.get("/tasks", {
         headers: {
           Authorization: localStorage.getItem('authToken')
@@ -76,16 +100,21 @@ export default function Modal({ isCreate, data, isOpen, onClose, children, title
             <div className="modal-body">
               <h2>Task description:</h2>
               <p>{data.description}</p>
-              
-              <h2>Status:</h2>
-              <SingleSelector
+
+              <h2>Status</h2>
+              <Select
                 required={true}
-                value={data.status}
-                options={statusOptions}
-                onChange={(e) => updateTaskStatus(data.id, e.target.value)}
+                styles={modalSelectorStyles}
+                options={Array.from(statusOptions.values())}
+                defaultValue={statusOptions.get(data.status)}
+                isRequired
+                onChange={(e) => updateTaskStatus(data.id, e)}
               />
-              
+
               <h2>Tags:</h2>
+              <div className="tags">
+                {data.tags.map((tag) => (<TagBadge name={tag.name} nameHex={tag.nameHex} backgroundHex={tag.backgroundHex} />))}
+              </div>
             </div>
             <footer className="modal-footer">
               <Button typeSubmit>
