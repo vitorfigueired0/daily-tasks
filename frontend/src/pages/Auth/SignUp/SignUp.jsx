bf2c243fbe2212343b1d4c97de37b1d0aab9af02
Vitor Figueiredo <vittor.figueiredo17@gmail.com>
2024-11-05 21:35:56 -0300

    [FE] Login/Sign up screen UI

--- frontend/src/pages/Auth/SignUp/SignUp.jsx
+++ frontend/src/pages/Auth/SignUp/SignUp.jsx
@@ -1,24 +1,61 @@
-import './SignUp.css'
+import { useState } from 'react';
+import './SignUp.css';
+import obucLogo from '../../../assets/obuc-logo.png';
 
 export const SignUp = () => {
+	const [isSignUp, setIsSignUp] = useState(false);
 
-	//00BE78
+	const toggleMode = () => {
+		setIsSignUp((prev) => !prev);
+	};
 
 	return (
 		<div id='main-wrapper'>
-			<div id="color-div">
-				
-				<div id="color-div-child">
-					<h1>Task Manager</h1>
-					<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque ad veritatis assumenda rerum inventore at accusantium minima fuga saepe repudiandae natus illo nisi, repellendus quidem consectetur perspiciatis temporibus vel! Eaque?</p>
-					<button>Already have account? Sign up</button>
+			<div id='form-div' className={isSignUp ? 'form-div-sigup' : 'form-div-sigin'}>
+				<div id='form-header'>
+					<figure><img src={obucLogo} alt='Logo' /></figure>
 				</div>
 
-			</div>
-			<div id="form-div">
-				2
+				<form className={isSignUp ? 'form-signup' : 'form-signin'}>
+					{isSignUp && (
+						<input
+							type='text'
+							className='login-input'
+							placeholder='Nome'
+						/>
+					)}
+					<input
+						type='email'
+						className='login-input'
+						placeholder='Email'
+					/>
+					<input
+						type='password'
+						className='login-input'
+						placeholder='Senha'
+					/>
+					{isSignUp && (
+						<input
+							type='password'
+							className='login-input'
+							placeholder='Confirmar Senha'
+						/>
+					)}
+
+					<button
+						type='submit'
+						className='submit-button'
+					>
+						{isSignUp ? 'Sing Up' : 'Sign In'}
+					</button>
+				</form>
+
+				<p onClick={toggleMode} className='toggle-mode'>
+					{isSignUp
+						? 'Already have an account? Login'
+						: `Don't have an account? Sign Up`}
+				</p>
 			</div>
 		</div>
-	)
-
-}
+	);
+};
